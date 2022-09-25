051f107cd78c247c9fd055081ea7dcfbd0a8e077
MahmoudiiOussama <oussama.mahmoudi01@esprit.tn>
2022-09-25 16:45:38 +0100

    update

--- api/main.py
+++ api/main.py
@@ -5,12 +5,26 @@ from PIL import Image
 import numpy as np
 import tensorflow as tf
 import requests
+from fastapi.middleware.cors import CORSMiddleware
 
-MODEL  = tf.keras.models.load_model("./models/2")
+MODEL  = tf.keras.models.load_model("./models/1")
 
 CLASS_NAMES = ["Early Blight", "Late Blight","Healthy"]
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 @app.get("/ping")
 async def ping():
     return "Hello, I am alive"
@@ -32,13 +46,13 @@ async def predict(
     json_data = {
         "instances": img_batch.tolist()
     }
+
     response = requests.post(endpoint, json=json_data)
     prediction = np.array(response.json()["predictions"][0])
     predicted_class = CLASS_NAMES[np.argmax(prediction)]
     confidence = np.max(prediction)
     
     return {
-        
         'class': predicted_class,
         'confidence': float(confidence)
     }  
